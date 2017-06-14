f9e8f7d3e3d83495bfb74a6a4a550a2d318342e8
DylanAndrew88 <Dandrewblake88@gmail.com>
2017-06-14 09:30:29 -0400

    button updates

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 
-var calcTitle = document.querySelector('#title');
-  alert('Change the Title');
+  var button7 = document.querySelector('#7');
+    onclick("calculator.anwer.value += '7'");
+
+  var button8 = document.querySelector('#8');
+    onclick("calculator.anwer.value += '8'");
+
+  var button9 = document.querySelector('#9');
+    onclick("calculator.anwer.value += '9'");
+
+  var buttonDivide = document.querySelector('#divide');
+    onclick("calculator.anwer.value += '1'");
