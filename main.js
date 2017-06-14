fc1c7408462a266c1c80785809ec1757224bf114
DylanAndrew88 <Dandrewblake88@gmail.com>
2017-06-13 22:46:02 -0400

    toying w/ js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
-    function substitute() {
-      var myValue = document.getElementById("myTextBox").value;
+      var myValue = document.querySelector("#myTextBox");
       if  (myValue.length == 0) {
         alert("Please enter a real value in the text box.");
         return;
       }
 
-      var myTitle = document.getElementById('title');
+      var myTitle = document.querySelector("#title");
         myTitle.innerHTML = myValue;
     }
