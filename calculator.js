d5570ead9f0310fbbb590e956704acb37960b34f
DylanAndrew88 <Dandrewblake88@gmail.com>
2017-06-13 23:12:16 -0400

    got js working!!

--- calculator.js
+++ calculator.js
@@ -1,10 +1,3 @@
 
-      var myValue = document.getElementById("#myTextBox");
-      if  (myValue.length == 0) {
-        alert.onclick("Please enter a real value in the text box.");
-        return;
-      }
-
-      var myTitle = document.getElementById("#title");
-        myTitle.innerHTML = myValue;
-    }
+var calcTitle = document.querySelector('#title');
+  alert('Change the Title');
